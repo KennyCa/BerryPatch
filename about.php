e358e3fd1c5577688d514e7596fc6097ec7abf5c
tidelkai <tiffany_baker@stu.indianhills.edu>
2018-03-05 16:30:50 -0600

    new
    bare bones

---
+++ about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="utf-8"> 
+	<title>Barry Patch About Us</title> 
+	<link rel="stylesheet" type="text/css" href="#" /> 
+	<!-- <link rel="stylesheet/less" type="text/css" href="styles.less" />
+	<script src="less.min.js" type="text/javascript"></script>-->
+</head> 
+ 
+<body>  
+	<div id="container">
+		<header><h1><i>Berry Patch IT Services and Computer Repair</i></h1></header>
+		
+		<nav>
+			<ul>
+				<li><a href="index.php">HOME<a></li>
+				<li><a href="#.php">SERVICES</a></li>
+				<li><a href="#.php">SHOP</a></li>
+				<li><a href="about.php">ABOUT US</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+		</nav><!--end nav-->
+
+<aside id="about-content">
+<h1>About Us</h1>
+<p>Berry Patch IT Services is a small I.T. business in Ottumwa, Iowa. We have professionally trained techs in Information Technology. Our techs love everything to do with the Technology Industry and are always completing additional training so they are up to date on the latest changes in the industry. We are a computer repair service Ottumwa can count on.<br>
+ 
+When it comes to computer repair, we know that you want it done fast, right, and at a good price. We will get your PC fixed quickly to your liking, and always at a reasonable price. We can do everything, from just cleaning out unwanted and unneeded software (bloatware), to even replacing or upgrading needed parts. <br>
+ 
+Berry Patch IT Services can build you a brand new custom built PC, or we also offer good quality refurbished computers. We have a wide variety of computer and computer related parts and cables for sale and can work to get you anything you need. We also sell Ethernet cables ordered to the length that you need. <br>
+ 
+Berry Patch IT Services in Ottumwa also will recycle that old computer you have and no longer need or want. It is not good for our landfill to have all of this e-junk in it and in some cases, it is against the law! So give us a call and we can come pick it up for you.<br>
+ 
+Ottumwa! If you have a computer question you need answered, just call us or send us an e-mail and we will give you the answer. We will answer all questions, from what is the best pc for you to how to use different software. If we don’t know the answer, we will find it and get back to you. <br>
+ 
+We are Ottumwa’s answer to computer needs and supplies.</p>
+</aside><!--end about-content-->
+
+<section id="hire-geek-col">
+	***
+</section><!-- end hire-a-geek-->
+
+<footer>
+	<aside id ="footer-Left-col">
+		Berry Patch IT Services <br>
+		"We Have A Passion For Technology!"
+	</aside><!--end footer-left-col-->
+	<div id="footer-copyright">
+		641-683-5754<br>
+		&copy2016 by Berry Patch IT Services.
+</div><!--end copyright-->
+
+</footer><!--end footer-->
+
+	</container><!--end container-->
+
+</body> 
+</html> 
---
+++ index.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html> 
+<html> 
+<head> 
+	<meta charset="utf-8"> 
+	<title>Barry Patch Home</title> 
+	<link rel="stylesheet" type="text/css" href="#" /> 
+	<!-- <link rel="stylesheet/less" type="text/css" href="styles.less" />
+	<script src="less.min.js" type="text/javascript"></script>-->
+</head> 
+ 
+<body>  
+	<div id="container">
+		<header><h1><i>Berry Patch IT Services and Computer Repair</i></h1></header>
+		
+		<nav>
+			<ul>
+				<li><a href="index.php">HOME<a></li>
+				<li><a href="#.php">SERVICES</a></li>
+				<li><a href="#.php">SHOP</a></li>
+				<li><a href="about.php">ABOUT US</a></li>
+				<li><a href="contact.php">CONTACT</a></li>
+			</ul>
+		</nav><!--end nav-->
+
+<div id="slogan">
+	<h2>Computer Problems?<br>
+	Need Computer Repair?<br>
+	Need a New or Used Computer?<br>
+	We are here to help you!<br></h2>
+    <h3>Call Us At: 641-683-5754 or 
+	641-777-4441.<br> 
+	<i>Servicing Ottumwa and the 
+	surrounding area since 2016!</i></h3>
+	
+</div><!--end slogan-->
+<div id="info">
+	With our experience in computer repair and data recovery, and a love for all things geek, we are Ottumwa’s solution to all your tech problems. Home & small business networking, computer repairs, back up, Computer Recycling, data recovery... Whatever your tech issue is, we've got Ottumwa and it surrounding area covered.<br>
+ 
+We know how intimidating your computer can be, so let us help. Why spend your day trying to solve your tech problems when one of our techs can get you online in no time? Call the Berry Patch IT Services in Ottumwa today with all of your computer needs!  Let us do the work so you can kick back and enjoy.
+
+</div><!--end info-->
+<div id="repair-info">
+	<h2>Phone & Tablet Screen Replacement</h2>
+ 
+<p>Berry Patch IT Services now replaces screens on all devices phone or Tablets. Screens are ordered as needed and take 2-3 days to arrive. It takes about 2 hours to replace the screen. Contact us for an estimate.</p>  
+</div><!--end repair-info-->
+
+<h2>Hire a Geek Today</h2>
+
+<div id="contact-form">
+  <form action="#">
+  	<div class="form-group">
+		<label for="first_name">First Name</label>
+		<input type="text" class="form-control" id="first_name" name="first_name">
+	</div>
+	<div class="form-group">
+		<label for="last_name">Last Name</label>
+		<input type="text" class="form-control" id="last_name" name="last_name">
+  	</div>
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+    </div>
+    <div class="form-group">
+      <label for="phone">Phone:</label>
+      <input type="#" class="form-control" id="phone" placeholder="Enter phone number" name="phone">
+    </div>
+    <button type="submit" class="btn btn-default">Submit</button>
+  </form><!-- end index-form-->
+</div><!-- end contact form-->
+<div id="testimonials">
+	<P>
+		It was a dark and stormy night and our 6 year old Dell crashed for the 20th time while I was desperately trying to print plane tickets. I had done everything possible to limp it along until I could save what I needed to off the dang thing. The 21st time it crashed, it was dead, dead, dead. In what my husband would later refer to as a fit of blinding rage, I dismantled that machine down to 50 itty bitty parts, a box and the hard drive. Only afterwards did I realize that now all of our contacts, tax information, photos, etc was gone. LUCKILY, I work with David's wife and she took the hard drive to David and in short order he saved all of my important items on a flash drive.<br>
+													Joan H.<br>
+													Oskaloosa, Iowa
+	</P>
+</div><!--end testimonials-->
+
+<footer>
+	<aside id ="footer-Left-col">
+		Berry Patch IT Services <br>
+		"We Have A Passion For Technology!"
+	</div><!-- end footer-left-col-->
+
+	<div id="footer-copyright">
+		641-683-5754<br>
+		&copy2016 by Berry Patch IT Services.
+</div><!--end copyright-->
+
+</footer><!--end footer-->
+
+	</container><!--end container-->
+
+</body> 
+</html> 
