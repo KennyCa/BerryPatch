e128e2c7309a701e888b4e9f7cd3a17a19273ecc
KennyCa <30661046+KennyCa@users.noreply.github.com>
2018-03-08 12:24:07 -0600

    admin page
    working on admin page

---
+++ class/itemclass.php
@@ -0,0 +1,17 @@
+<?php namespace newitem;
+
+class newitem {
+	$name = "na";
+	$description = "na";
+	$cost = 0;
+	$imagename = "na";
+	$imagepath= "na";
+
+public function construct () {
+
+}	
+
+public function construct ()
+	
+}
+?>
