0ae422f25b2e79ba7fd61406770b1a8c857f5b3b
tidelkai <tiffany_baker@stu.indianhills.edu>
2018-03-14 12:58:51 -0500

    fixed starter php

--- BP-Starter.php
+++ BP-Starter.php
@@ -8,30 +8,54 @@
 <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
-<nav id="myNavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top role="navigation">
-    <!-- grouping -->
-    <div class="container-fluid">
-        <div class="navbar-header col-sm-5 col-xs4" style="padding-bottom: 10px;">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-            <a class="navbar-brand"><i>Berry Patch IT Services and Computer Repair</i></a>
-        </div>
-        <!--collections Nav for toggle-->
-        <div class="collapse navbar-collapse" id="navbarCollapse">
-            <ul class="nav navbar-nav">
-                <li class="active"><a href="index.php" >HOME</a></li>
-                <li><a href="services.php">SERVICES</a></li>
-                <li><a href="shop.php">SHOP</a></li>
-                <li><a href="about.php">ABOUT</a></li>
-                <li><a href="contact.php">CONTACT</a></li>
-            </ul>
-        </div>
-    </div>
-</nav>
+                <!--header with nav-->
+                    <nav id="myNavbar" class="navbar navbar-default navbar-inverse navbar-fixed-top role="navigation">
+                        <!-- grouping -->
+                        <div class="container-fluid">
+                            <div class="navbar-header col-sm-5 col-xs4" style="padding-bottom: 10px;">
+                                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#navbarCollapse">
+                                    <span class="sr-only">Toggle navigation</span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                    <span class="icon-bar"></span>
+                                </button>
+                                <a class="navbar-brand"><i>Berry Patch IT Services and Computer Repair</i></a>
+                            </div>
+                            <!--collections Nav for toggle-->
+                            <div class="collapse navbar-collapse" id="navbarCollapse">
+                                <ul class="nav navbar-nav">
+                                    <li class="active"><a href="index.php" >HOME</a></li>
+                                    <li><a href="services.php">SERVICES</a></li>
+                                    <li><a href="shop.php">SHOP</a></li>
+                                    <li><a href="about.php">ABOUT</a></li>
+                                    <li><a href="contact.php">CONTACT</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
 
+
+
+
+<!--footer-->
+    
+                <div class="row">
+                    <div class="col-sm-9" style="background-color:#000000; color:#ffffff;">
+                        <footer>
+                            <p>641-683-5754
+                                <br>
+                            Berry Patch IT Services
+                        </p>
+                        </footer>
+                    </div>
+                    <div class="col-sm-3" style="background-color:#000000; color:#ffffff;">
+                        <footer>
+                            <br>
+                        
+                            <p>&copy; 2016 by Berry Patch IT Services.</p>
+                        </footer>
+                    </div>
+                </div>
+            </div>
 </body>
 </html>
